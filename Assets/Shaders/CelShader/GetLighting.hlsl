ff1d8c77823aaa2e554f32e615b9cffc2dfc352d
Kirijo <asyelsub@gmail.com>
2021-02-14 12:02:07 -0500

    uhhhhh.....?

--- Assets/Shaders/CelShader/GetLighting.hlsl
+++ Assets/Shaders/CelShader/GetLighting.hlsl
@@ -1,6 +1,6 @@
 void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half Attenuation)
 {
-#if SHADERGRAPH_PREVIEW
+#if defined (SHADERGRAPH_PREVIEW)
 	Direction = half3(0.5, 0.5, 0);
 	Color = 1;
 	Attenuation = 1;
