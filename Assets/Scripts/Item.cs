76fbed6ac9afd7945beab757d4b4199f5d98bac4
Kirijo <asyelsub@gmail.com>
2021-03-11 14:44:41 -0500

    item script

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Goes on an object in the world that can be picked up
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    string ID;
+
+    string name;
+    string UIDesc;
+    GameObject UIModel;
+    string hoverText;
+    bool isCollected;
+    bool isPlaced;
+    bool isBook;
+
+    public Item(string ID)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
