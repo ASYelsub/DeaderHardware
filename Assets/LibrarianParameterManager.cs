7283959ff18d335ab31c454c123125b9b115c4d1
FINNJC <finnjcarney@gmail.com>
2021-03-16 22:17:25 -0400

    AHHHHHHHHHHH

---
+++ Assets/LibrarianParameterManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class LibrarianParameterManager : MonoBehaviour
+{
+    public GameObject[] librarians;
+
+    private Transform player;
+    private float dis;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (int i = 0; i < librarians.Length; i++)
+        {
+            dis = Mathf.Min(dis, Vector3.Distance(player.position, librarians[i].transform.position));
+        }
+        Debug.Log("dis = " + dis);
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("LibRange", dis);
+        dis = 1000;
+    }
+}
