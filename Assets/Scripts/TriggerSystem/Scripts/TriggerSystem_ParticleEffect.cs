331beec1f28347afe634abdb1ef3e405e2ac8969
FINNJC <finnjcarney@gmail.com>
2021-04-02 16:27:45 -0400

    Added a new script to handle Particle Effects only, adjust particle effect for Console Entity, Added to Hub Scene

---
+++ Assets/Scripts/TriggerSystem/Scripts/TriggerSystem_ParticleEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSystem_ParticleEffect : MonoBehaviour, ITriggerable
+{
+    public ParticleSystem ps;
+
+    public void ExecuteTriggerFunction()
+    {
+        ps.Play();
+    }
+
+
+    public void ExecuteLeaveTriggerFunction()
+    {
+        ps.Stop();
+    }
+
+}
