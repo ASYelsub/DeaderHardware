60f994741d8093788ed52cf5bb726ee4dbd1de7b
Unknown <atd335@nyu.edu>
2021-04-15 12:41:43 -0400

    Popup integration

--- Assets/Scripts/TriggerSystem/Scripts/TriggerSystem_ParticleEffect.cs
+++ Assets/Scripts/TriggerSystem/Scripts/TriggerSystem_ParticleEffect.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class TriggerSystem_ParticleEffect : MonoBehaviour, ITriggerable
 {
     public ParticleSystem ps;
-    //
+    public string hoverText;
+
     public void ExecuteTriggerFunction()
     {
         ps.Play();
+        if (GameObject.FindGameObjectWithTag("POPUP"))
+        {
+            GameObject.FindGameObjectWithTag("POPUP").GetComponent<PopupTextScript>().StartUp(hoverText);
+        }
     }
 
 
     public void ExecuteLeaveTriggerFunction()
     {
         ps.Stop();
+        if (GameObject.FindGameObjectWithTag("POPUP"))
+        {
+            GameObject.FindGameObjectWithTag("POPUP").GetComponent<PopupTextScript>().Stop();
+        }
     }
+
 
 }
