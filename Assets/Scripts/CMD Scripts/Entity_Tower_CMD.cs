f41023de7a465c73a039107232da8141f1e09961
Abigail <asyelsub@gmail.com>
2021-05-05 12:50:50 -0400

    dialogue trigger stuff working in hub and library

--- Assets/Scripts/CMD Scripts/Entity_Tower_CMD.cs
+++ Assets/Scripts/CMD Scripts/Entity_Tower_CMD.cs
@@ -6,10 +6,22 @@ public class Entity_Tower_CMD : MonoBehaviour, IDialogueCommand
 {
     [SerializeField]
     private TowerPopUp_Trigger tower;
+
+    [SerializeField]
+    private bool isTower;
+    [SerializeField]
+    private GameObject toDisable;
+    [SerializeField] private GameObject toEnable;
     public void ExcecuteDialogueCommand()
     {
         Debug.Log("Checking Execute Dialogue Command");
         tower.ExecuteTriggerFunction();
+
+        if (isTower)
+        {
+            toEnable.SetActive(true);
+            toDisable.SetActive(false);
+        }
     }
 
     
